80748113da94159482746c9a52200d1e3f717e92
MalgorzataO85 <m.ostachowska@microsoft.wsei.edu.pl>
2019-09-16 20:02:19 +0200

    Abstrakcja - interfejsy i klasy abstrakcyjne

--- DataTypes/Person.cs
+++ DataTypes/Person.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace DataTypes
 {
-    class Person
+    public struct Person
     {
         public string Name;
         public string LastName { get; set; } //właściwość
         public int? BirthDate { get; set; } //? - może być null
 
-        public Person()
-        {
-            BirthDate = 1950; //konstuktor, wartość domyślna chroni przed wyjątkami, np. żeby nie było nulla
-        }
-
+        
         public int GetAge()
         {
             return DateTime.Today.Year - (int)BirthDate;   //metoda
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -10,32 +10,42 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
-            ////Console.WriteLine("Hello Worls");
-            ////bool isTrue = true;
-            ////isTrue = false;
-            //int a = 10;
-            //decimal b = a;
-            ////int c = Convert.ToInt32(b); //konwersja, może być też rzutowanie
-            ////int c = int.Parse(b.ToString());
-            //int c = 1;
-            //string d = "Roman";
+            TyrFor();
+            //////Console.WriteLine("Hello Worls");
+            //////bool isTrue = true;
+            //////isTrue = false;
+            ////int a = 10;
+            ////decimal b = a;
+            //////int c = Convert.ToInt32(b); //konwersja, może być też rzutowanie
+            //////int c = int.Parse(b.ToString());
+            ////int c = 1;
+            ////string d = "Roman";
 
-            //int e = a / c;
-            ////a++; inkrementacje
-            ////++a;
-            ///
+            ////int e = a / c;
+            //////a++; inkrementacje
+            //////++a;
+            /////
             Person roman = new Person();
             roman.Name = "Roman";
             roman.LastName = "Nowak";
             roman.BirthDate = 2000;
-
-            Person anna = roman;
+            object o = new object();
+            var anna = roman;
+            if (anna.Equals(roman))
+            {
+                Console.WriteLine("ok");
+            }
             anna.Name = "Anna";
 
-            //int c = 10;
-            //int d = c;
-            //Console.WriteLine(c);
-            //Console.WriteLine(d);
+            //dynamic x = 45.5m;
+            //x = "Imie";
+            //x = 10;
+            //x = true;
+            //Console.WriteLine(x);
+            ////int c = 10;
+            ////int d = c;
+            ////Console.WriteLine(c);
+            ////Console.WriteLine(d);
 
             Console.WriteLine(roman.Name);
             Console.WriteLine(roman.LastName);
@@ -45,6 +55,64 @@ namespace DataTypes
             Console.WriteLine(anna.LastName);
             Console.WriteLine(anna.GetAge());
             Console.ReadLine();
+        }
+
+        private static void TyrFor()
+        {
+            //for (int i = 0; i < 10; i++) //gdy chcemy dokładnż ilość razy wyonać pętlę
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int i = 0;           
+            //while (i < 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    break; // return; contunue;
+            //    i++;
+            //}
+            //while (i < 0);
+
+            //int[] ints = { 1, 2, 3, 4 };
+            //foreach (var item in ints)
+            //{
+            //    var x = item * 10;
+            //    Console.WriteLine(x);
+            //}
+
+            //int i = 0;
+            //if(i<10)
+            //{
+            //    return;
+            //}
+            //else if(i>10)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //else
+            //    Console.WriteLine(i *= 10);
+
+            //int i = 100;
+
+            //switch (i)
+            //{
+            //    case 10:
+            //        Console.WriteLine(10);
+            //        goto case 100;
+            //    case 100:
+            //        Console.WriteLine(100);
+            //        break;
+            //    default:
+            //        Console.WriteLine(i *= 10);
+            //        break;
+            //}
+
         }
     }
 }
