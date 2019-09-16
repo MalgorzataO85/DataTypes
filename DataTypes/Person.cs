c879cd9c6ffb863211adca886a9adc9361b43c4a
Małgorzata Ostachowska <m.ostachowska@microsoft.wsei.edu.pl>
2019-09-16 18:30:25 +0200

    Add project files.

---
+++ DataTypes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Person
+    {
+        public string Name;
+        public string LastName { get; set; } //właściwość
+        public int? BirthDate { get; set; } //? - może być null
+
+        public Person()
+        {
+            BirthDate = 1950; //konstuktor, wartość domyślna chroni przed wyjątkami, np. żeby nie było nulla
+        }
+
+        public int GetAge()
+        {
+            return DateTime.Today.Year - (int)BirthDate;   //metoda
+        }
+
+
+    }
+}
---
+++ DataTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Console.WriteLine("Hello Worls");
+            ////bool isTrue = true;
+            ////isTrue = false;
+            //int a = 10;
+            //decimal b = a;
+            ////int c = Convert.ToInt32(b); //konwersja, może być też rzutowanie
+            ////int c = int.Parse(b.ToString());
+            //int c = 1;
+            //string d = "Roman";
+
+            //int e = a / c;
+            ////a++; inkrementacje
+            ////++a;
+            ///
+            Person roman = new Person();
+            roman.Name = "Roman";
+            roman.LastName = "Nowak";
+            roman.BirthDate = 2000;
+
+            Person anna = roman;
+            anna.Name = "Anna";
+
+            //int c = 10;
+            //int d = c;
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+
+            Console.WriteLine(roman.Name);
+            Console.WriteLine(roman.LastName);
+            Console.WriteLine(roman.GetAge());
+
+            Console.WriteLine(anna.Name);
+            Console.WriteLine(anna.LastName);
+            Console.WriteLine(anna.GetAge());
+            Console.ReadLine();
+        }
+    }
+}
